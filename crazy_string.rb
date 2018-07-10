918fe129c734774aa6feab53a280a43090466903
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-10 17:53:04 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -2,13 +2,13 @@
 first_word = "hello"
 second_word = "friends"
 def crazy_strings(first_word,second_word)
-puts "#{first_word.upcase.reverse},#{second_word.upcase.reverse}"
+puts "#{first_word.upcase.reverse}"
 end
 crazy_strings(first_word,second_word)
 
 first_word = "hello"
-second_word = "friends"
+
 def crazy_strings(first_word,second_word)
-puts "#{first_word.gsub("s","z").swapcase},#{second_word.gsub("s","z").swapcase}"
+puts "#{second_word.gsub("s","z").swapcase}"
 end
 crazy_strings(first_word,second_word)

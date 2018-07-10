b0f376c31e8f8c4e096c26f5291e197fac13d323
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-10 17:48:42 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -9,6 +9,6 @@ crazy_strings(first_word,second_word)
 first_word = "hello"
 second_word = "friends"
 def crazy_strings(first_word,second_word)
-puts "#{first_word.swapcase.gsub("s","z")},#{second_word.swapcase.gsub("s","z")}
+puts "#{first_word.swapcase.gsub("s","z")},#{second_word.swapcase.gsub("s","z")}"
 end
 crazy_strings(first_word,second_word)

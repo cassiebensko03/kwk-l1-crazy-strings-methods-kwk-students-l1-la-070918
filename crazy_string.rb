bc2ff8354d7af5753f8bdc9e38c0e81dc19af7e5
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-10 17:54:09 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -2,7 +2,7 @@
 first_word = "hello"
 second_word = "friends"
 def crazy_strings(first_word,second_word)
-puts "#{first_word.upcase.reverse}"
+puts "#{first_word.reverse.upcase}"
 end
 crazy_strings(first_word,second_word)
 

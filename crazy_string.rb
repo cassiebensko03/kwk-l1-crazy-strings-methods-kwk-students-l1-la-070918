bb011fbe72cb73c9919539980185f619431ff488
cassiebensko03 <cassidymbensko@gmail.com>
2018-07-10 17:40:28 +0000

    automatically backed up by learn

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,7 @@
-crazy_strings
 
+first_word = "hello"
+second_word = "friends"
 def crazy_strings(first_word,second_word)
-puts #{first_word} 
+puts "#{first_word},#{second_word}"
 end
+crazy_strings
